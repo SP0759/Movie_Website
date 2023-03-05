24f1699b24aef0aa0be7694ae6a43e5eefa7e036
arjun <kshitijlavhe11@gmail.com>
2023-03-05 12:16:42 +0530

    initial commit

---
+++ movie_downloader/urls.py
@@ -0,0 +1,27 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from movie_downloader  import views
+from django. conf import settings
+from django. conf.urls.static import static
+urlpatterns = [
+    path("",views.index, name='movie_downloader'),
+    path("login.html",views.login, name='logon'),
+    path("about",views.about, name='about'),
+    path("signup.html",views.signup, name='signup'),
+    path("register",views.register,name='register'),
+    path("login",views.loginn,name='login'),
+    path("home",views.home,name='home'),
+    path("movies",views.movie,name="movie"),
+    path("add",views.add,name="addmovie"),
+    path("logout",views.logoutt,name="logout"),
+    path('player/<str:tlink>/<str:name>/<str:description>', views.player, name='player'),
+
+
+
+
+
+
+
+]
+ 
